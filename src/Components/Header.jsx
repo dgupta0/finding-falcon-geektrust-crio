c0b31a9abce474d1af210850724bd9ec1dbccede
Deeti <deetigupta8@gmail.com>
2023-09-28 16:18:48 +0530

    final touches

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,10 +3,12 @@ import * as React from 'react';
 import Button from '@mui/material/Button';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
-import menu from "../assets/menu.png"
+import menu from "../assets/menu.png";
+import { useLocation } from "react-router-dom";
 
 export default function Header(){
-    const [anchorEl, setAnchorEl] = React.useState(null);
+ const [anchorEl, setAnchorEl] = React.useState(null);
+ const location = useLocation();
   const open = Boolean(anchorEl);
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
@@ -14,30 +16,36 @@ export default function Header(){
   const handleClose = () => {
     setAnchorEl(null);
   };
-    function handleHeaderReset(){
+    function handleHeaderReset(){  
     localStorage.removeItem("data");
-    localStorage.removeItem("result")
+    localStorage.removeItem("result");
+    window.location.reload();
     }
     return(
     <header>
             <h1 className="title">
             Finding Falcone
             </h1>
-            <nav className="larger-nav">
-                <Link className="nav-link" to="/">rules</Link>
-                <Link onClick={handleHeaderReset} className="nav-link" to="/game">reset</Link>
-                <Link className="nav-link" to="game">game</Link>
-            </nav>
+                {location.pathname === "/" ?
+                <nav className="larger-nav">
+                 <Link onClick={handleHeaderReset} className="nav-link play-now" to="game">Play Now</Link>
+                 </nav>
+                 :
+                 <nav className="larger-nav">
+                 <Link className="nav-link" to="/">Rules</Link>
+                <Link onClick={handleHeaderReset} className="nav-link" to="/game">Reset</Link>
+                </nav>
+                }
             <nav className="smaller-nav">
             <Button
-        id="demo-positioned-button"
-        aria-controls={open ? 'demo-positioned-menu' : undefined}
-        aria-haspopup="true"
-        aria-expanded={open ? 'true' : undefined}
-        onClick={handleClick}
-      >
-       <img width={"30px"} src={menu} alt="Icon by ariefstudio<"/>
-      </Button>
+            id="demo-positioned-button"
+            aria-controls={open ? 'demo-positioned-menu' : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? 'true' : undefined}
+            onClick={handleClick}
+            >
+            <img width={"30px"} src={menu} alt="Icon by ariefstudio<"/>
+            </Button>
       <Menu
         id="demo-positioned-menu"
         aria-labelledby="demo-positioned-button"
@@ -53,15 +61,20 @@ export default function Header(){
           horizontal: 'left',
         }}
       >
+          {location.pathname === "/" ?
+          <MenuItem onClick={handleClose}>
+          <Link onClick={handleHeaderReset} className="nav-link" to="game">Play Now</Link>
+          </MenuItem>
+        :
+        <>
         <MenuItem onClick={handleClose}>
-        <Link className="nav-link" to="/">rules</Link>
+        <Link onClick={handleHeaderReset} className="nav-link" to="/game">Reset</Link>
         </MenuItem>
         <MenuItem onClick={handleClose}>
-        <Link onClick={handleHeaderReset} className="nav-link" to="/game">reset</Link>
+        <Link className="nav-link" to="/">Rules</Link>
         </MenuItem>
-        <MenuItem onClick={handleClose}>
-        <Link className="nav-link" to="game">game</Link>
-        </MenuItem>
+        </>
+        }
       </Menu>
             </nav>
             </header>
