e793878df7aad4b79d712bf77207ffaf68c955c1
Deeti <deetigupta8@gmail.com>
2023-11-17 14:18:48 +0530

    rectifying reset from game page

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,41 +2,39 @@ import { Link } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 
-export default function Header(){
- const location = useLocation();
- const navigate = useNavigate()
-  
-    function handleHeaderReset(){  
+export default function Header() {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  function handleHeaderReset() {
     localStorage.removeItem("data");
     localStorage.removeItem("result");
-    location.pathname === "/game" ? window.location.reload() :  navigate("/game")
-   }
-   
-    return(
+    navigate("/game");
+  }
+
+  return (
     <header>
-            <h1 className="title">
-            Finding Falcone
-            </h1>
-                {location.pathname === "/" ?
-                <nav className="larger-nav">
-                 <Link onClick={handleHeaderReset} className="nav-link play-now" to="game">Play Now</Link>
-                 </nav>
-                 :
-                 <nav className="larger-nav">
-                 <Link className="nav-link" to="/">Rules</Link>
-                <Link onClick={handleHeaderReset} className="nav-link" to="/game">Reset</Link>
-                </nav>
-                }
-           
-            </header>
-        ) 
-        
+      <h1 className="title">Finding Falcone</h1>
+      {location.pathname === "/" ? (
+        <nav className="larger-nav">
+          <Link
+            onClick={handleHeaderReset}
+            className="nav-link play-now"
+            to="game"
+          >
+            Play Now
+          </Link>
+        </nav>
+      ) : (
+        <nav className="larger-nav">
+          <Link className="nav-link" to="/">
+            Rules
+          </Link>
+          <Link onClick={handleHeaderReset} className="nav-link" to="/game">
+            Reset
+          </Link>
+        </nav>
+      )}
+    </header>
+  );
 }
-
-           
-          
-
-
-
-  
-    
